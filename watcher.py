9cf3e075d9364fa05b82517ee16b70d40b0ee6d2
Artiya Thinkumpang <artiya4u@gmail.com>
2019-03-18 13:00:27 +0700

    Check if email in content.

--- watcher.py
+++ watcher.py
@@ -105,7 +105,7 @@ def get_mime_message(service, user_id, msg_id):
 
 def extract_email_address(message):
     emails = re.search(r"[\w\.-]+@[\w\.-]+", message)
-    if emails:
+    if emails is None:
         return False
 
     return emails.group()
